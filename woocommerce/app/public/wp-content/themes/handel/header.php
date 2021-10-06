105cdd863c098c1bd4da47bfbcca9b48d752e995
Thales Mendonca <thalesmendonca@id.uff.br>
2021-10-06 18:24:41 -0300

    header e barra de navegacao prontos

--- woocommerce/app/public/wp-content/themes/handel/header.php
+++ woocommerce/app/public/wp-content/themes/handel/header.php
@@ -4,7 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name')?> | <?php the_title();?></title>
+    <title><?php bloginfo('name')?> <?php wp_title('|');?></title>
     <?php wp_head();?>
 </head>
 <body <?php body_class();?>>
+
+<?php
+    $img_url = get_stylesheet_directory_uri() . '/img';
+    $cart_count = WC()->cart->get_cart_contents_count();
+?>
+
+<header class='header container'>
+    <a href=""><img src="<?= $img_url; ?>/handel.svg" alt="Handel"></a>
+    <div class="busca">
+        <form action="<?php bloginfo('url');?>/loja/" method="get">
+            <input type="text" name="s" id="s" placeholder="Buscar" value="<?php the_search_query(); ?>">
+            <input type="text" name="post_type" id="product" class="hidden">
+            <input type="submit" id="searchbutton" value="Buscar">
+        </form>
+    </div>
+    <nav class="conta">
+        <a href="/minha-conta" class="minha-conta">Minha Conta</a>
+        <a href="/carrinho" class="carrinho">Carrinho
+            <?php if($cart_count) { ?>
+                <span class="carrinho-count"> <?= $cart_count?> </span>
+            <?php } ?>
+        </a>
+    </nav>
+</header>
+
+<?php
+    wp_nav_menu([
+        'menu' => 'categorias',
+        'container' => 'nav',
+        'container_class' => 'menu-categorias'
+    ]);
+?>
